97ade5a7fad682541ee6ca8b57bf06c5482daaf8
crummy <crummynz@gmail.com>
2017-04-11 19:42:47 +0200

    Improve separation algorithm. Sliding scale.

--- src/com/malcolmcrum/flocking/Instincts/Separation.java
+++ src/com/malcolmcrum/flocking/Instincts/Separation.java
@@ -5,38 +5,28 @@ import processing.core.PVector;
 
 import java.util.Set;
 
+import static processing.core.PApplet.map;
 import static processing.core.PConstants.PI;
 import static processing.core.PVector.angleBetween;
 import static processing.core.PVector.dist;
 
 public class Separation extends Instinct {
-	private static final float dangerCloseDistance = 16;
-	private static final float tooCloseDistance = 32;
-	private static final float closeAngle = PI/4; // if angle between two close birds is outside this, we don't consider them close - they'll be gone soon
-
 	public Separation(Boid self, Set<Boid> boids) {
 		super(self, boids);
 	}
 
 	@Override
 	public Desire calculateDesire() {
-		float urgency = 0;
 		PVector awayFromOthers = new PVector();
 		for (Boid other : getNeighbours()) {
 			float distance = dist(self.position, other.position);
 
-			float velocityAngles = angleBetween(self.velocity, other.velocity);
-			boolean onSameCourse = velocityAngles < closeAngle;
-			if (distance < dangerCloseDistance && onSameCourse) {
-				urgency = 0.5f;
-			} else if (distance < tooCloseDistance && onSameCourse) {
-				urgency = 0.1f;
-			}
+			float urgency = map(distance, 0, getNeighbourRadius(), 1, 0);
 
 			float angle = angleBetween(self.velocity, other.velocity);
-			awayFromOthers.add(PVector.fromAngle(angle + PI));
+			awayFromOthers.add(PVector.fromAngle(angle + PI).mult(urgency*urgency));
 		}
 
-		return new Desire(urgency, awayFromOthers);
+		return new Desire(1, awayFromOthers);
 	}
 }
